c93b34e9d8791aab083beaef386d4dec5e9c0af8
betogroo <luizhumberto@gmail.com>
2021-05-05 20:01:21 -0300

    refactor: rename Home to Tasks

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Tasks from '../views/Tasks.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Tasks',
+    component: Tasks
   },
   {
     path: '/about',
