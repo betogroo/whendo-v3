802c4b5e768318b4cd79f3e7c5eda73455d487ec
betogroo <luizhumberto@gmail.com>
2021-05-05 19:31:59 -0300

    feat: create AppBar component

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-menu left bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="item in navRightMenu"
+          :key="item.title"
+          @click="() => {}"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+
+  props: {
+    title: {
+      type: String,
+      default: 'Totas as Tarefas'
+    }
+  },
+
+  data: () => ({
+    navRightMenu: [
+      { title: 'Ordenar' },
+      { title: 'Desmarcar Tarefas Concluídas' },
+      { title: 'Excluir Tarefas Concluídas' },
+      { title: 'Esconder Tarefas Concluídas' },
+      { title: 'Enviar' }
+    ]
+  })
+}
+</script>
+
+<style></style>
