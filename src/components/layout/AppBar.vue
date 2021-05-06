a6400ae3896edc4e2ab51fb8e2096c399bbec84d
betogroo <luizhumberto@gmail.com>
2021-05-05 21:49:59 -0300

    feat: create drawer

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>{{ title }}</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-    <v-menu left bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item
-          v-for="item in navRightMenu"
-          :key="item.title"
-          @click="() => {}"
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </v-app-bar>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="item in navRightMenu"
+            :key="item.title"
+            @click="() => {}"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" temporary></v-navigation-drawer>
+  </div>
 </template>
 
 <script>
@@ -33,11 +35,12 @@ export default {
   props: {
     title: {
       type: String,
-      default: 'Totas as Tarefas'
+      default: 'Todas as Tarefas'
     }
   },
 
   data: () => ({
+    drawer: false,
     navRightMenu: [
       { title: 'Ordenar' },
       { title: 'Desmarcar Tarefas Concluídas' },
@@ -45,7 +48,13 @@ export default {
       { title: 'Esconder Tarefas Concluídas' },
       { title: 'Enviar' }
     ]
-  })
+  }),
+
+  methods: {
+    toggleDrawer() {
+      this.$emit('toggleDrawer')
+    }
+  }
 }
 </script>
 
