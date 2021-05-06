d32df8fb7ff48ae4908fd37812a84a8ae2dfc82a
betogroo <luizhumberto@gmail.com>
2021-05-06 16:29:41 -0300

    feat: profile and taskLists - undone

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -24,7 +24,45 @@
         </v-list>
       </v-menu>
     </v-app-bar>
-    <v-navigation-drawer app v-model="drawer" temporary width="272"></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer" temporary width="272">
+      <div id="profile">
+        <v-card color="primary" dark tile>
+          <v-list dense class="ml-0 pt-0">
+            <v-list-item two-line>
+              <v-list-item-avatar size="48">
+                <img :src="localAccount.imageProfile" />
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ localAccount.name }}</v-list-item-title>
+                <v-list-item-subtitle v-if="isLogged"
+                  >Logged In</v-list-item-subtitle
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item class="justify-end pt-0">
+              <v-icon @click="toggleOptions" dark medium
+                >mdi-chevron-{{ toggleOptionsButton ? 'up' : 'down' }}</v-icon
+              >
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </div>
+      <div id="drawer-menu">
+        <v-list dense>
+          <v-list-item-group v-model="selectedItem" color="primary">
+            <v-list-item v-for="(item, i) in allTaskLists" :key="i">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </div>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -40,19 +78,50 @@ export default {
   },
 
   data: () => ({
-    drawer: false,
+    toggleOptionsButton: 0,
+    isLogged: 0,
+    loggedUser: {
+      name: 'Luiz Roberto Cintra',
+      imageProfile: 'https://randomuser.me/api/portraits/men/81.jpg',
+      email: 'luizRoberto@gmail.com'
+    },
+    localAccount: {
+      name: 'Conta Local',
+      imageProfile:
+        'https://www.pngarts.com/files/10/Default-Profile-Picture-Download-PNG-Image.png'
+    },
+    drawer: true,
     navRightMenu: [
       { title: 'Ordenar' },
       { title: 'Desmarcar Tarefas Concluídas' },
       { title: 'Excluir Tarefas Concluídas' },
       { title: 'Esconder Tarefas Concluídas' },
       { title: 'Enviar' }
-    ]
+    ],
+    selectedItem: 1,
+    defaultItems: [
+      { text: 'Todas as Tarefas', icon: 'mdi-calendar' },
+      {
+        text: 'Tarefas Finalizadas',
+        icon: 'mdi-checkbox-marked-circle-outline'
+      }
+    ],
+    taskLists: [{ text: 'Todas as Tarefas', icon: 'mdi-menu', border: 'red' }]
   }),
 
   methods: {
     toggleDrawer() {
       this.$emit('toggleDrawer')
+    },
+    toggleOptions() {
+      this.toggleOptionsButton = !this.toggleOptionsButton
+    }
+  },
+
+  computed: {
+    allTaskLists() {
+      const tasklists = [...this.defaultItems, ...this.taskLists]
+      return tasklists
     }
   }
 }
