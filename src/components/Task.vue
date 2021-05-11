9d00bdeafcab00983cd74169065af94d025aa2d8
betogroo <luizhumberto@gmail.com>
2021-05-11 17:53:34 -0300

    feat: create task component

---
+++ src/components/Task.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-list-item class="pl-0" :style="`border-left: 4px solid ${task.color}`">
+      <v-list-item-action @click="checkItem()" class="d-flex ma-0 px-3 mb-3">
+        <v-icon
+          >mdi-{{
+            task.isDone ? 'checkbox-marked-outline' : 'checkbox-blank-outline'
+          }}</v-icon
+        >
+      </v-list-item-action>
+      <v-list-item-content
+        class="pa-0 pt-1"
+        :class="
+          task.isDone ? 'text-decoration-line-through text--disabled' : ''
+        "
+      >
+        <v-list-item-title v-text="task.title"></v-list-item-title>
+        <v-list-item-subtitle
+          v-text="task.note"
+          class="d-inline-block text-truncate"
+          style="max-width: 250px"
+        ></v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    checkItem() {
+      this.$emit('checkItem')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-list--two-line .v-list-item {
+  min-height: auto;
+}
+</style>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -2,13 +2,27 @@
   <div>
     <tasks-sort-bar></tasks-sort-bar>
     <button-float class="mb-4" @action="goTo('About')"></button-float>
-    <h4 class="text-center font-weight-regular text--disabled">
-      Nenhuma tarefa Adicionada
-    </h4>
+    <template v-if="tasks.length > 0">
+      <v-list dense two-line>
+        <task
+          @checkItem="checkItem(task)"
+          v-for="task in tasks"
+          :key="task.id"
+          :task="task"
+        />
+      </v-list>
+    </template>
+    <template v-else>
+      <h4 class="text-center font-weight-regular text--disabled">
+        Nenhuma tarefa Adicionada
+      </h4>
+    </template>
   </div>
 </template>
 
 <script>
+import TaskServices from '@/services/TaskServices.js'
+import Task from '@/components/Task.vue'
 import ButtonFloat from '@/components/layout/ButtonFloat.vue'
 import TasksSortBar from '@/components/layout/widgets/TasksSortBar.vue'
 export default {
@@ -16,18 +30,35 @@ export default {
 
   components: {
     ButtonFloat,
-    TasksSortBar
+    TasksSortBar,
+    Task
   },
 
   data: () => ({
-    btnAddTask: false
+    btnAddTask: false,
+    tasks: []
   }),
 
   methods: {
     // tests
     goTo(routeName) {
       this.$router.push({ name: routeName })
+    },
+    checkItem(data) {
+      const i = this.tasks.findIndex((item) => item.id === data.id)
+      const isDone = !this.tasks[i].isDone
+      this.$set(this.tasks, i, { ...this.tasks[i], isDone })
     }
+  },
+
+  created() {
+    TaskServices.getTasks()
+      .then((res) => {
+        this.tasks = res.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 }
 </script>
